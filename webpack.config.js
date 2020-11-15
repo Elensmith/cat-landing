a3e1430a3ef5f8da2128a9461b9d490087c6db60
Elena Kuznetsova <elenakuznetsova177@yandex.ru>
2020-11-15 21:39:59 +0300

    header and webpack

---
+++ webpack.config.js
@@ -0,0 +1,131 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackMd5Hash = require("webpack-md5-hash");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+// const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+//   .BundleAnalyzerPlugin;
+// подключаем плагин
+const isDev = process.env.NODE_ENV === "development";
+module.exports = {
+  entry: { main: "./src/index.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    // filename: (chunkData) => {
+    //   return chunkData.chunk.name === "main"
+    //     ? "[name].[hash].js"
+    //     : "[name]/[name].[hash].js";
+    // },
+    filename: "[name].[hash].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: "file-loader?name=./vendor/[name].[ext]",
+      },
+
+      {
+        test: /\.css$/i,
+        use: [
+          isDev
+            ? { loader: "style-loader" }
+            : {
+              loader: MiniCssExtractPlugin.loader,
+              options: { publicPath: "../" },
+            },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'postcss-preset-env',
+                    {
+                      // Options
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+        ],
+      },
+
+      {
+        test: /\.(png|jpg|gif|ico|svg)$/,
+        use: [
+          "file-loader?name=./images/[name].[ext]", // указали папку, куда складывать изображения
+          {
+            loader: "image-webpack-loader",
+            // options: {
+            //   mozjpeg: {
+            //     progressive: true,
+            //     quality: 65,
+            //   },
+            //   // optipng.enabled: false will disable optipng
+            //   optipng: {
+            //     enabled: false,
+            //   },
+            //   pngquant: {
+            //     quality: [0.65, 0.9],
+            //     speed: 4,
+            //   },
+            //   gifsicle: {
+            //     interlaced: false,
+            //   },
+            //   // the webp option will enable WEBP
+            //   webp: {
+            //     quality: 75,
+            //   },
+            // },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    // new BundleAnalyzerPlugin(),
+    new MiniCssExtractPlugin({
+      //
+      filename: "[name].[hash].css",
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require("cssnano"),
+      cssProcessorPluginOptions: {
+        preset: ["default"],
+      },
+      canPrint: true,
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: "./src/index.html",
+      filename: "index.html",
+      chunks: ["main"],
+    }),
+    new WebpackMd5Hash(),
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+    }),
+  ],
+};
